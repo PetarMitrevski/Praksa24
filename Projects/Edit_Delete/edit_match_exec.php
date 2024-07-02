e03da4e63e04243b2cbc309956504a4e1ee83108
PetarMitrevski <kk.petarmitrevski1@gmail.com>
2024-07-02 14:01:16 +0200

    Changed

--- Projects/Edit_Delete/edit_match_exec.php
+++ Projects/Edit_Delete/edit_match_exec.php
@@ -48,21 +48,21 @@ $conn->close();
 exit;
 
 function getMatchById($conn, $id) {
-    $stmt = $conn->prepare("SELECT * FROM matches WHERE matchID = ?");
+    $stmt = $conn->prepare("SELECT * FROM matches WHERE matchID = ?;");
     $stmt->bind_param("i", $id);
     $stmt->execute();
     return $stmt->get_result()->fetch_assoc();
 }
 
 function getTeamById($conn, $teamID) {
-    $stmt = $conn->prepare("SELECT * FROM teams WHERE teamID = ?");
+    $stmt = $conn->prepare("SELECT * FROM teams WHERE teamID = ?;");
     $stmt->bind_param("i", $teamID);
     $stmt->execute();
     return $stmt->get_result()->fetch_assoc();
 }
 
 function updateMatch($conn, $id, $week, $homeTeam, $awayTeam, $homeScore, $awayScore, $matchDate, $matchTime) {
-    $stmt = $conn->prepare("UPDATE matches SET week = ?, HomeTeamID = ?, AwayTeamID = ?, HomeScore = ?, AwayScore = ?, matchDate = ?, matchTime = ? WHERE matchID = ?");
+    $stmt = $conn->prepare("UPDATE matches SET week = ?, HomeTeamID = ?, AwayTeamID = ?, HomeScore = ?, AwayScore = ?, matchDate = ?, matchTime = ? WHERE matchID = ?;");
     $stmt->bind_param("iiiiissi", $week, $homeTeam, $awayTeam, $homeScore, $awayScore, $matchDate, $matchTime, $id);
     $stmt->execute();
 }
@@ -91,37 +91,37 @@ function updateDraw($conn, $homeTeam, $awayTeam, $homeRow, $awayRow, $oldMatch) 
 }
 
 function adjustDrawToWin($conn, $teamID) {
-    $stmt = $conn->prepare("UPDATE teams SET Wins = Wins + 1, Points = 3 * Wins + Draws - 1, Draws = Draws - 1 WHERE teamID = ?");
+    $stmt = $conn->prepare("UPDATE teams SET Wins = Wins + 1, Points = 3 * Wins + Draws - 1, Draws = Draws - 1 WHERE teamID = ?;");
     $stmt->bind_param("i", $teamID);
     $stmt->execute();
 }
 
 function adjustDrawToLoss($conn, $teamID) {
-    $stmt = $conn->prepare("UPDATE teams SET Losses = Losses + 1, Draws = Draws - 1, Points = 3 * Wins + Draws WHERE teamID = ?");
+    $stmt = $conn->prepare("UPDATE teams SET Losses = Losses + 1, Draws = Draws - 1, Points = 3 * Wins + Draws  WHERE teamID = ?;");
     $stmt->bind_param("i", $teamID);
     $stmt->execute();
 }
 
 function adjustLossToWin($conn, $teamID) {
-    $stmt = $conn->prepare("UPDATE teams SET Wins = Wins + 1, Points = 3 * Wins + Draws, Losses = Losses - 1 WHERE teamID = ?");
+    $stmt = $conn->prepare("UPDATE teams SET Wins = Wins + 1, Points = 3 * Wins + Draws, Losses = Losses - 1 WHERE teamID = ?;");
     $stmt->bind_param("i", $teamID);
     $stmt->execute();
 }
 
 function adjustWinToLoss($conn, $teamID) {
-    $stmt = $conn->prepare("UPDATE teams SET Losses = Losses + 1, Wins = Wins - 1, Points = 3 * Wins + Draws  WHERE teamID = ?");
+    $stmt = $conn->prepare("UPDATE teams SET Losses = Losses + 1, Wins = Wins - 1, Points = 3 * Wins + Draws  WHERE teamID = ?;");
     $stmt->bind_param("i", $teamID);
     $stmt->execute();
 }
 
 function adjustWinToDraw($conn, $teamID) {
-    $stmt = $conn->prepare("UPDATE teams SET Wins = Wins - 1, Draws = Draws + 1, Points = 3 * Wins + Draws  WHERE teamID = ?");
+    $stmt = $conn->prepare("UPDATE teams SET Wins = Wins - 1, Draws = Draws + 1, Points = 3 * Wins + Draws  WHERE teamID = ?;");
     $stmt->bind_param("i", $teamID);
     $stmt->execute();
 }
 
 function adjustLossToDraw($conn, $teamID) {
-    $stmt = $conn->prepare("UPDATE teams SET Draws = Draws + 1, Losses = Losses - 1, Points = 3 * Wins + Draws  WHERE teamID = ?");
+    $stmt = $conn->prepare("UPDATE teams SET Draws = Draws + 1, Losses = Losses - 1, Points = 3 * Wins + Draws  WHERE teamID = ?;");
     $stmt->bind_param("i", $teamID);
     $stmt->execute();
 }

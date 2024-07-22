84dfa2cb8c4d60bd1feddbe93c204220378d26a4
PetarMitrevski <kk.petarmitrevski1@gmail.com>
2024-07-22 15:58:47 +0200

    login in progress

--- Projects/Edit_Delete/check_login.php
+++ Projects/Edit_Delete/check_login.php
@@ -1,6 +1,40 @@
 <?php
 
-require_once "../PHP_data/config.php";
+
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+
+    require_once "../PHP_data/config.php";
+
+    $username = htmlspecialchars($_POST['username']);
+    $password = htmlspecialchars($_POST['password']);
+    $repeated_password = htmlspecialchars($_POST['password_repeat']);
+
+
+    $sql = "SELECT * FROM users WHERE UserName = ? AND Pass = ?";
+
+    if($password !== $repeated_password)
+    header("Location: ../index.php?error=passwords_dont_match");
+
+    else{
+        
+        $statement = $conn->prepare($sql);
+        $statement->bind_param("ss", $username, $password);
+        $statement->execute();
+        $response = $statement->get_result();
+        $result = $response->fetch_assoc();
+
+        
+        if($result)
+        header("Location: ../home.php?user=$result[UserName]");
+        
+       
+    }
+}
+
+
+
+
+
 
 
 

a1f521f378ad5efe65a32e4a0819623fb19784ed
PetarMitrevski <kk.petarmitrevski1@gmail.com>
2024-08-01 11:59:35 +0200

    Changed

---
+++ Projects/PHP_data/functions.php
@@ -0,0 +1,32 @@
+<?php
+
+
+function destroySession(){
+
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+    
+    session_destroy();
+    session_unset();
+}
+
+
+function checkLogin($status, $user){
+
+    if(!isset($status) && !isset($user)){
+     
+        header("Location: index.php");
+     
+        destroySession();
+     
+         exit;    
+     
+     }
+}
+
+
